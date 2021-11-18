088207b13c81457370914ba606088466da910c87
namsi-lia <lydianamsi@gmail.com>
2021-11-18 12:31:16 +0300

    Wether app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  openWeatherApiKey: '{902062cf6f68394dceed153fade4b709}',
+  firebase: {
+    apiKey: "AIzaSyCjs0WNK5mmbaune565gXxYHdkY_d2IOWs",
+    authDomain: "weatherapp-325313.firebaseapp.com",
+    projectId: "weatherapp-325313",
+    storageBucket: "weatherapp-325313.appspot.com",
+    messagingSenderId: "27903905071",
+    appId: "1:27903905071:web:59574a248b117b11d7a5da",
+    measurementId: "G-FNFMCMBTW3"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production:true,
+  openWeatherApiKey: '902062cf6f68394dceed153fade4b709',
+  firebase: {
+  apiKey: "AIzaSyCjs0WNK5mmbaune565gXxYHdkY_d2IOWs",
+  authDomain: "weatherapp-325313.firebaseapp.com",
+  projectId: "weatherapp-325313",
+  storageBucket: "weatherapp-325313.appspot.com",
+  messagingSenderId: "27903905071",
+  appId: "1:27903905071:web:59574a248b117b11d7a5da",
+  measurementId: "G-FNFMCMBTW3"
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
